aa818e100252f948bf869416e6ca4c69df6af743
MadsDocs <waterfoxbrowser@gmail.com>
2016-02-22 13:06:44 +0100

    MySQL Installation entfernt, isValid Domain hinzugefügt
    Init.cs: MySQL Connector installation wurde entfernt
    misc: isValid Funktion hinzugefügt

--- Classes/Init.cs
+++ Classes/Init.cs
@@ -33,8 +33,6 @@ namespace ADUser.Classes
                                 -- Updates
                                     --Changelogs
 
-                Desweiteren prüft die Methode ob der MySQL Connector installiert ist und
-                wenn dieser nicht installiert ist, wird dieser installiert. 
 
         */
         public static void init()
@@ -56,61 +54,6 @@ namespace ADUser.Classes
                     Directory.CreateDirectory(variablen.appdata + @"\UserData\Updates\Changelogs");
 
                     logger._ilogger("Directory wurde erfolgreich erstellt..");
-                    logger._ilogger("Versuche nun den MySQL Connector zu installieren");
-
-                    WebClient mysql = new WebClient();
-                    mysql.DownloadFile(@"\\srv00\Deployment\MySQL Connector\mysql-connector-net-6.9.8.msi", variablen.appdata + @"\UserData\Temp\mysql-connector-net-6.9.8.msi");
-                    Process.Start(variablen.appdata + @"\UserData\Temp\mysql-connector-net-6.9.8.msi", "/quiet");
-
-                    if(Directory.Exists(@"C:\Program Files (x86)\MySQL"))
-                    {
-                        Console.WriteLine("MySQL Connector wurde installiert... Das Programm kann nun verwenden werden!");
-                        logger._ilogger("MySQL Connector wurde installiert... Überprüfung des Ordner war positiv (Untersuchter Ordner: C:\\Program Files (x86)\\MySQL");
-                        variablen.can_start = true;
-                        logger._ilogger("Neuer ADUser Start Status ist: " + variablen.can_start);
-
-                    }
-                    else
-                    {
-                        
-
-                        var result = MessageBox.Show(variablen.message, variablen.caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                        if (result == DialogResult.Yes)
-                        {
-                            Console.WriteLine("Wir versuchen noch mal eine Automatische Installation.");
-                            WebClient connector = new WebClient();
-                            connector.DownloadFile(@"\\srv00\Deployment\MySQL Connector\mysql-connector-net-6.9.8.msi", variablen.appdata + @"\UserData\Temp\mysql-connector-net-6.9.8.msi");
-                            Process.Start(variablen.appdata + @"\UserData\Temp\mysql-connector-net-6.9.8.msi", "/quiet");
-
-                            if(!Directory.Exists(@"C:\Program Files (x86)\MySQL"))
-                            {
-                                MessageBox.Show(variablen.error_message, variablen.error_caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                                variablen.can_start = false;
-                                logger._sLogger(variablen.can_start);
-                                Environment.Exit(-1);
-
-                            }
-                            else
-                            {
-                                Console.WriteLine("MySQL Connector wurde installiert, das Programm kann nun verwendet werden!");
-                                variablen.can_start = true;
-                                logger._sLogger(variablen.can_start);
-                            }
-
-
-
-                        }
-                        else
-                        {
-                            MessageBox.Show(variablen.error_message, variablen.error_caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                            logger._wlogger("MySQL Connector konnte nicht installiert werden! Bitte den MySQL Connector manuell installieren!");
-                            variablen.can_start = false;
-                            logger._sLogger(variablen.can_start);
-                        }
-
-                        
-                    }
-
                 }
             }
             catch (Exception ex)
--- Classes/misc.cs
+++ Classes/misc.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.DirectoryServices.ActiveDirectory;
+using System.Net.NetworkInformation;
 
 namespace ADUser.Classes
 {
@@ -73,6 +75,43 @@ namespace ADUser.Classes
 
             variablen.isValid = true;
             return variablen.isValid;
+        }
+
+        public static void isValidDomain ()
+        {
+            try
+            {
+                Ping pingsender = new Ping();
+                PingOptions options = new PingOptions(128, true);
+                string data = "aaaaaaaaaaaaaaaaaaa";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                PingReply reply = pingsender.Send(Classes.variablen.domain);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Domain wurde gefunden!");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else if (reply.Status == IPStatus.TimedOut)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Domain wurde nicht gefunden!");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+            catch (Exception ex)
+            {
+                Classes.logger._elogger(ex);
+            }
+          
+            
+        }
+
+        public static void ShowDomain()
+        {
+            Console.WriteLine(Environment.UserDomainName);
+            Console.ReadLine();
         }
     }
 }
